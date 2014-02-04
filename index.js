cf6ffda466357f6dbc7ca54f9b4fb2d9267cd421
Ben Drucker <bvdrucker@gmail.com>
2014-02-04 15:44:54 -0500

    Switch request for next -- clearer terminology

--- index.js
+++ index.js
@@ -5,22 +5,22 @@ module.exports = {
   version: require('./package.json').version,
   register: function (plugin, options, next) {
 
-    plugin.ext('onPreResponse', function (request, reply) {
+    plugin.ext('onPreResponse', function (request, next) {
       var Hapi = plugin.hapi,
       response = request.response;
 
       if (response.variety === 'plain' && typeof response.source.then === 'function') {
         return response.source.then(function (res) {
-          return reply(res);
+          return next(res);
         }, function (err) {
           if (!err.isBoom) {
             err = Hapi.error.internal(err.message, err);
           }
-          return reply(err);
+          return next(err);
         });
       }
 
-      reply();
+      next();
     });
 
     next();
